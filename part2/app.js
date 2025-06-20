aa6093104cd021ae47874189bb772d43f262ff6f
Zch6111 <zhang041005@gmail.com>
2025-06-20 07:47:30 +0000

    [automatic commit] 2025-06-20 07:47:30 UTC 129.127.202.29

--- part2/app.js
+++ part2/app.js
@@ -70,4 +70,12 @@ app.get('/api/my-dogs', (req, res) => {
         .catch((err) => res.status(500).json({ error: 'Database error' }));
 });
 
+// Get current logged-in user info
+app.get('/api/users/me', (req, res) => {
+    if (!req.session.user) {
+        return res.status(401).json({ error: 'Not logged in' });
+    }
+    res.json({ user_id: req.session.user.id, username: req.session.user.username, role: req.session.user.role });
+});
+
 module.exports = app;
