934c85c9f18c37a09a0cb7a186a88b2813c4de7b
Zch6111 <zhang041005@gmail.com>
2025-06-20 05:44:23 +0000

    [automatic commit] 2025-06-20 05:44:23 UTC 129.127.202.29

--- part2/app.js
+++ part2/app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
+const session = require('express-session');
 
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(session({
+    secret: 'dogwalksecret',
+    resave: false,
+    saveUninitialized: true
+}));
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
